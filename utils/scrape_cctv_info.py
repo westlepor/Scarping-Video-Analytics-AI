25eb5fd54714590517637fc2d03af2789425434c
westlepor <westlepor984@gmail.com>
2020-04-01 13:49:20 +0200

    saved cctv info as csv

--- utils/scrape_cctv_info.py
+++ utils/scrape_cctv_info.py
@@ -8,6 +8,7 @@ import json
 import re
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 def get_cctv_url():
     """ Scrape cctv live url from js variable
@@ -22,12 +23,16 @@ def get_cctv_url():
     # Get js file location which contain cctv information
     cctv_info_url = 'http://cwwp2.dot.ca.gov/vm/' + data[2]['src']
 
-    # Parse js variable to get urls
+    # Parse js variable to get urls array
     cctv_info_content = requests.get(cctv_info_url).text
     cctv_urls = []
     for line in cctv_info_content.splitlines():
         url = line[line.find('http') : line.find('htm') + 3]
         if url:
             cctv_urls.append(url)
+
+    with open('cctv_info.csv', 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerow(cctv_urls)
 
     return cctv_urls
