84e99003daf2e4d50d3811684633fb91c4ec3132
westlepor <westlepor984@gmail.com>
2020-04-01 15:42:45 +0200

    remove ffmpeg

--- utils/scrape_image.py
+++ utils/scrape_image.py
@@ -8,7 +8,7 @@ import cv2
 import subprocess as sp
 import numpy
 import re
-from scrape_cctv_info import get_cctv_url
+from utils.scrape_cctv_info import get_cctv_url
 
 def get_image():
     FFMPEG_BIN = "ffmpeg.exe"
@@ -17,21 +17,11 @@ def get_image():
     m3u8_url = raw_source[raw_source.find("sourceURL") + len("sourceURL") + 3 : raw_source.find("posterFrameURL") - 3]
     video_url = re.sub(r'[^a-zA-Z0-9:_/.]', '', m3u8_url)
 
-    pipe = sp.Popen([ FFMPEG_BIN, "-i", video_url,
-            "-loglevel", "quiet",
-            "-an",
-            "-f", "image2pipe",
-            "-pix_fmt", "bgr24",
-            "-vcodec", "rawvideo", "-"],
-            stdin = sp.PIPE, stdout = sp.PIPE)
-
     while True:
-        raw_image = pipe.stdout.read(640*480*3)
-        image =  numpy.fromstring(raw_image, dtype='uint8').reshape((480,640,3))
-        cv2.imwrite("test.jpg", image) 
-        if cv2.waitKey(5) == 27:
+        video = cv2.VideoCapture(video_url)
+        ret, frame = video.read()
+        cv2.imwrite("test.jpg", frame)
+        if cv2.waitKey() == 27:
             break
-
-    pipe.stdout.flush()
 
     cv2.destroyAllWindows()
